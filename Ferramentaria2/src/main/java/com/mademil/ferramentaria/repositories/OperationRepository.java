2102e4304a13a97ab4cac9520420e0e166217950
Gregori-Rodrigues <gregori.rodriguesmademil@gmail.com>
2024-11-28 08:06:32 -0300

    Delete Ferramentaria2 directory

--- Ferramentaria2/src/main/java/com/mademil/ferramentaria/repositories/OperationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mademil.ferramentaria.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.mademil.ferramentaria.entities.Operation;
-
-public interface OperationRepository extends JpaRepository<Operation, Integer> {
-
-}
